bfa35ef29599a4fc82dd950f67e767a55f410e59
SagYernar <sagyernar@gmail.com>
2017-09-10 02:04:31 +0600

    Домашнее задание по теме индексация.
    Создать индексатор для одномерного массива который при установке значения будет возводить в квадрат передаваемое значение переменной и устанавливать его для указанного индекса. При получении элемента массива по индексу будет возвращаться его текущее значение.

---
+++ ConsoleApp4/ConsoleApp4/MassivVKvadrate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class MassivVKvadrate
+    {
+        private int[] massiv;
+
+        public MassivVKvadrate(int size)
+        {
+            massiv = new int[size];
+        }
+
+        public int this[int i]
+        {
+            get { return massiv[i]; }
+            set { massiv[i] = value*value; }
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int sizeOfMassiv, number;
+
+            Console.WriteLine("Введите размер массива");
+            str = Console.ReadLine();
+            for (; ; )
+            {
+                if (Int32.TryParse(str, out sizeOfMassiv))
+                {
+                    sizeOfMassiv = Int32.Parse(str);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Недопустимый символ! Пожалуйста убедитесь, что вводите целое число");
+                    Console.WriteLine("Введите размер массива");
+                    str = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Введите число");
+            str = Console.ReadLine();
+
+            for (; ; )
+            {
+                if (Int32.TryParse(str, out number))
+                {
+                    number = Int32.Parse(str);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Недопустимый символ! Пожалуйста убедитесь, что вводите целое число");
+                    Console.WriteLine("Введите число");
+                    str = Console.ReadLine();
+                }
+            }
+
+            MassivVKvadrate massiv = new MassivVKvadrate(sizeOfMassiv);
+
+            massiv[0] = number;
+
+            Console.WriteLine("Квадрат числа {0} = {1}", number, massiv[0]);
+            Console.ReadLine(); 
+        }
+    }
+}
